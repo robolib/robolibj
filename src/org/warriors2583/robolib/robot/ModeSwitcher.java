9946482a34e47bbbef10398a8c37efde2b6546dd
noriah <vix@noriah.dev>
2014-04-13 10:20:44 -0500

    Added DSInfo.java Driver Station Info is retrieved from DSInfo.java Driver Station LCD is written to through DSInfo.java

--- src/org/warriors2583/robolib/robot/ModeSwitcher.java
+++ src/org/warriors2583/robolib/robot/ModeSwitcher.java
@@ -22,10 +22,16 @@ import java.util.Map;
 
 /**
  * Handles the Switching of the Game mode and the Execution of the mode.
+ * This controls what Mode the robot should run.
+ * It handles feeding the Watchdog and running the Scheduler.
+ * 
  * @author noriah Reuland
  */
 public class ModeSwitcher {
     
+    /**
+     * 
+     */
     public static class GameMode{
         
         private final int value;
@@ -34,7 +40,15 @@ public class ModeSwitcher {
             this.value = value;
             this.name = name;
         }
+        /**
+         * Get the value of the GameMode
+         * @return the GameMode Value
+         */
         public int getValue(){ return value; }
+        /**
+         * Get then name of the GameMode
+         * @return the GameMode Name
+         */
         public String getName(){ return name; }
         
         public static final GameMode kNoMode = new GameMode(0, "NoMode");
@@ -50,8 +64,9 @@ public class ModeSwitcher {
     private static final String NETTABLE_CURRENT_MODE = "mode";
     private static final String NETTABLE_CURRENT_MODE_STRING = "mode-string";
     
-    private GameMode currentMode;
+    private GameMode m_currentMode;
     
+    private boolean m_initialized;
     private final Map m_modes = new HashMap();
     private final Scheduler m_scheduler;
     
@@ -62,14 +77,34 @@ public class ModeSwitcher {
     }
     
     private ModeSwitcher(){
-        currentMode = GameMode.kNoMode;
+        m_initialized = false;
+        m_currentMode = GameMode.kNoMode;
         m_scheduler = Scheduler.getInstance();
     }
     
+    /**
+     * Add the a {@link RobotMode} to the ModeSwitcher this will overwrite any previous
+     * mode that has the same {@link GameMode}.
+     * @param mType
+     * @param rMode 
+     */
     protected void add(GameMode mType, RobotMode rMode){
         m_modes.put(rMode, mType);
     }
     
+    /**
+     * Initializes the Game Mode Switcher.
+     * This method must be called when ready to start the robot.
+     * 
+     * Checks each Mode to see if there is code to run, if not, a Default class
+     * will be added.
+     *
+     * @see RobotMode
+     * @see DisabledMode
+     * @see TestMode
+     * @see AutonMode
+     * @see TeleopMode
+     */
     protected void init(){
         if(!m_modes.containsKey(GameMode.kDisabled)){
             Robot.debug("No Disabled Robot Mode Defined");
@@ -109,18 +144,37 @@ public class ModeSwitcher {
                 public void run(){ Timer.delay(0.001); }
                 public void end(){}  
             };
-        }        
+        }
+        m_currentMode = GameMode.kDisabled;
+        m_initialized = true;
     }
     
+    /**
+     * Runs the Current RobotMode.
+     */
     protected void run(){
         getRobotMode()._run();
         m_scheduler.run();
     }
     
+    /**
+     * Gets the current {@link GameMode}.
+     * 
+     * @return the current {@link GameMode} 
+     */
     public GameMode getGameMode(){
-        return currentMode;
+        return m_currentMode;
     }
     
+    /**
+     * Gets the current {@link RobotMode}.
+     * 
+     * If there is no current Mode, then something went wrong,
+     * and an error mode is returned that will cause the robot to quit.
+     * 
+     * 
+     * @return the current {@link RobotMode}
+     */
     public RobotMode getRobotMode(){
         if(m_modes.size() <= 0){
             return new RobotMode(){
@@ -131,27 +185,55 @@ public class ModeSwitcher {
                 public void end(){}    
             };
         }
-        return (RobotMode) m_modes.get(currentMode);
+        return (RobotMode) m_modes.get(m_currentMode);
     }
     
-    public void switchMode(int value){
-        switchMode(GameMode.kModes[value]);
+    /**
+     * Switch the current {@link GameMode}.
+     * 
+     * This will first call the end() Method of the current {@link RobotMode},
+     * then set the current {@link GameMode}, call the System Garbace Collector,
+     * and then call the new RobotMode init() method.
+     * 
+     * @param mode the {@link GameMode} number to switch to.
+     */
+    public void switchMode(int mode){
+        switchMode(GameMode.kModes[mode]);
     }
     
+    /**
+     * Switch the current {@link GameMode}.
+     * 
+     * This will first call the end() Method of the current {@link RobotMode},
+     * then set the current {@link GameMode}, call the System Garbace Collector,
+     * and then call the new RobotMode init() method.
+     * 
+     * @param mode the {@link GameMode} to switch to.
+     */
     public void switchMode(GameMode mode){
         getRobotMode()._end();
-        currentMode = mode;
+        m_currentMode = mode;
         Robot.getRobotTable().putNumber(NETTABLE_CURRENT_MODE, mode.getValue());
         Robot.getRobotTable().putString(NETTABLE_CURRENT_MODE_STRING, mode.getName());
         System.gc();
         getRobotMode()._init();
     }
     
+    /**
+     * Gets whether the mode given is different to the current running mode
+     * @param mode the mode number of the comparing {@link GameMode}
+     * @return a boolean value, true if we are in a new mode or false if we are in the same mode
+     */
     public boolean inNewMode(int mode){
-        return currentMode.getValue() != mode;
+        return m_currentMode.getValue() != mode;
     }
     
+    /**
+     * Gets whether the mode given is different to the current running mode
+     * @param mode the {@link GameMode} we are comparing
+     * @return a boolean value, true if we are in a new mode or false if we are in the same mode
+     */
     public boolean inNewMode(GameMode mode){
-        return currentMode != mode;
+        return m_currentMode != mode;
     }
 }
