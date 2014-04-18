d38dbd3b9b6be3804286891c7af94db74bd2e81d
Austin <amreuland@gmail.com>
2014-04-18 18:01:50 -0500

    Changed Package Declaration
    changed from org.warriors2583.robotlib to org.wwr.robolib

---
+++ src/org/wwr/robolib/robot/TestMode.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2014 Westwood Robotics code.westwoodrobotics@gmail.com.
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ */
+
+package org.wwr.robolib.robot;
+
+import edu.wpi.first.wpilibj.communication.FRCControl;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+/**
+ *
+ * @author Austin Reuland
+ */
+public abstract class TestMode extends RobotMode {
+    
+    protected TestMode(){
+        super(ModeSwitcher.GameMode.kTest);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected final void modeInit(){
+        LiveWindow.setEnabled(true);
+        init();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected final void modeRun(){
+        FRCControl.observeUserProgramTest();
+        run();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected final void modeEnd(){
+        LiveWindow.setEnabled(false);
+        end();
+    }
+
+}
