2720c3475caa0581bdfbb9394caf4675bc3c2083
noriah <vix@noriah.dev>
2015-02-14 20:48:04 -0600

    New Single Action Commands

---
+++ src/io/github/robolib/command/SingleActionCommand.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2015 noriah <vix@noriah.dev>.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ */
+
+package io.github.robolib.command;
+
+/**
+ * A command that runs one thing, then finishes
+ *
+ * @author noriah Reuland <vix@noriah.dev>
+ */
+public abstract class SingleActionCommand extends Command {
+    
+    public SingleActionCommand(){
+        super();
+    }
+    
+    public SingleActionCommand(String name){
+        super(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void initialize() {
+        doAction();
+    }
+    
+    protected abstract void doAction();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void execute() {}
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected boolean isFinished() { return true; }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void end() {}
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void interrupted() {}
+
+    
+    
+}
